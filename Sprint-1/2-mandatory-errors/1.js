1ea32299ef9c651d1edcf01867be275428217091
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2025-10-22 13:20:37 +0200

    Update 1.js
    Made changes according to the requirement

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,7 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
-age = age + 1;
+let age = 33;   // use 'let' instead of 'const'
+age = age + 1;  // reassigns 'age' by adding 1
+
+console.log(age); // Output: 34
+
