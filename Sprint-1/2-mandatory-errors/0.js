a2ba7fc380fea257156235e17640190e1edfd15f
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2025-10-22 13:17:43 +0200

    Update 0.js
    added sign

--- Sprint-1/2-mandatory-errors/0.js
+++ Sprint-1/2-mandatory-errors/0.js
@@ -1,2 +1,4 @@
-This is just an instruction for the first activity - but it is just for human consumption
-We don't want the computer to run these 2 lines - how can we solve this problem?
+// This is just an instruction for the first activity - but it is just for human consumption
+// We don't want the computer to run these 2 lines - how can we solve this problem?
+
+ //added // sign in begining of each comment to make sure that its is a comment not a part of the code.
