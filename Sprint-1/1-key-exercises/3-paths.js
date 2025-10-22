2aabe374312d336e3f480c5871687cd6ccc4d080
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2025-10-22 13:10:14 +0200

    Update 3-paths.js
    updated the code

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -10,14 +10,19 @@
 // (All spaces in the "" line should be ignored. They are purely for formatting.)
 
 const filePath = "/Users/mitch/cyf/Module-JS1/week-1/interpret/file.txt";
+
+// Find where the last slash is — that’s the boundary between the directory and the file
 const lastSlashIndex = filePath.lastIndexOf("/");
+
+// The "base" is everything after the last slash
 const base = filePath.slice(lastSlashIndex + 1);
 console.log(`The base part of ${filePath} is ${base}`);
 
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
+const dir = filePath.slice(0, lastSlashIndex); // everything before the last slash
+const ext = base.slice(base.lastIndexOf(".")); // everything after the last dot, including the dot
 
-// https://www.google.com/search?q=slice+mdn
+console.log(`The dir part of ${filePath} is ${dir}`);
+console.log(`The ext part of ${filePath} is ${ext}`);
