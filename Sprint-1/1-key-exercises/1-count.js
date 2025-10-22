b504e208268565146cdf389e3e703844216536b9
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2025-10-22 12:51:19 +0200

    Update 1-count.js
    explained the functionality of line 3

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -3,4 +3,4 @@ let count = 0;
 count = count + 1;
 
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
-// Describe what line 3 is doing, in particular focus on what = is doing
+//Line 3 is adding 1 to the current value of count and stores the result back into count
