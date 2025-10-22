2c807263617bbb7d98a79d53c2c3dc1db190ef24
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2025-10-22 13:29:26 +0200

    Update 4.js
    updated Hour Format

--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,2 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const hour12ClockTime = "08:53 PM"; // 12-hour format should include AM/PM
+const hour24ClockTime = "20:53";    // 24-hour format
