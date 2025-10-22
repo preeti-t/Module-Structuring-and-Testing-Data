e23adbef0e6f3eca72a18f8fa985f107c6fb9a4d
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2025-10-22 13:22:56 +0200

    Update 2.js
    Fixed Execution Order

--- Sprint-1/2-mandatory-errors/2.js
+++ Sprint-1/2-mandatory-errors/2.js
@@ -1,5 +1,4 @@
-// Currently trying to print the string "I was born in Bolton" but it isn't working...
-// what's the error ?
+const cityOfBirth = "Bolton";           // declared and initialized
+console.log(`I was born in ${cityOfBirth}`);  // variable now accessible
 
-console.log(`I was born in ${cityOfBirth}`);
-const cityOfBirth = "Bolton";
+//Fixed Execution Order
