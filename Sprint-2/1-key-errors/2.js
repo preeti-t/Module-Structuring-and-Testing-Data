45a776f575e7fefbb2f3f8755f0d2a0fd2131745
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2025-10-22 23:47:16 +0200

    Update 2.js
    updated as required

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -3,18 +3,25 @@
 
 // this function should square any number but instead we're going to get an error
 
-// =============> write your prediction of the error here
+// Variable should be declared as "num" and later value should be passed to "num" > write your prediction of the error here
 
-function square(3) {
+/*function square(3) {
     return num * num;
-}
+}*/
 
-// =============> write the error message here
+// SyntaxError: Unexpected number > write the error message here
 
-// =============> explain this error message here
+// can’t put a number (3) as a parameter name.
+//Function parameters must be identifiers (like num, x, or value), not literal values.
+//returning num * num, but num is never defined> explain this error message here
 
 // Finally, correct the code to fix the problem
 
-// =============> write your new code here
+function square(num) {
+  return num * num;
+}
+
+console.log(square(3)); 
+
 
 
