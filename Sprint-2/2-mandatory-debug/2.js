aeb2ef6a08a0747ba73604a3192b87d86bab8cc7
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2025-10-25 00:51:55 +0200

    Update 2.js
    updated the correct code with explaination

--- Sprint-2/2-mandatory-debug/2.js
+++ Sprint-2/2-mandatory-debug/2.js
@@ -1,9 +1,9 @@
 // Predict and explain first...
 
 // Predict the output of the following code:
-// =============> Write your prediction here
+// console.log should be return only 103> Write your prediction here
 
-const num = 103;
+/*const num = 103;
 
 function getLastDigit() {
   return num.toString().slice(-1);
@@ -11,14 +11,28 @@ function getLastDigit() {
 
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
-console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);*/
 
 // Now run the code and compare the output to your prediction
-// =============> write the output here
+/*The last digit of 42 is 3
+The last digit of 105 is 3
+The last digit of 806 is 3> write the output here*/
 // Explain why the output is the way it is
-// =============> write your explanation here
+// because we have provided a constant value 103 to num> write your explanation here
 // Finally, correct the code to fix the problem
-// =============> write your new code here
 
-// This program should tell the user the last digit of each number.
-// Explain why getLastDigit is not working properly - correct the problem
+function getLastDigit(num) {
+  return num.toString().slice(-1);
+}
+
+console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+
+// This program should tell the user the last digit of each number: yes
+/* here is the new output
+The last digit of 42 is 2
+The last digit of 105 is 5
+The last digit of 806 is 6*/
+// now I have defined a function that takes one argument, num. also removed const funtion
