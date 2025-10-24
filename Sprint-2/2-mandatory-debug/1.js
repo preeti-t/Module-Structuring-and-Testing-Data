c1ed1c7fe15104ed586a8c43a83884a26174d7ce
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2025-10-25 00:35:11 +0200

    Update 1.js
    updated function call

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,19 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  with return we should give some value that comes in the output> write your prediction here
 
-function sum(a, b) {
+/*function sum(a, b) {
   return;
   a + b;
 }
 
-console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);*/
 
-// =============> write your explanation here
+// a+b should be used after return in line 5> write your explanation here
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+
+function sum(a, b) {
+  return a + b;
+  
+}
+
+console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
