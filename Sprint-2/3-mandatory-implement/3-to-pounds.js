cbc7db4f73e4e7a580b2ba06eb827d7062136eb7
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2025-10-25 13:04:04 +0200

    Update 3-to-pounds.js
    Code updated - based on requirements

--- Sprint-2/3-mandatory-implement/3-to-pounds.js
+++ Sprint-2/3-mandatory-implement/3-to-pounds.js
@@ -4,3 +4,30 @@
 // You will need to declare a function called toPounds with an appropriately named parameter.
 
 // You should call this function a number of times to check it works for different inputs
+function toPounds(penceString) {
+  // Remove the trailing 'p'
+  const penceStringWithoutTrailingP = penceString.substring(
+    0,
+    penceString.length - 1
+  );
+
+  // Make sure it's at least 3 digits (e.g., '9' -> '009')
+  const paddedPenceNumberString = penceStringWithoutTrailingP.padStart(3, "0");
+
+  // Split into pounds and pence parts
+  const pounds = paddedPenceNumberString.substring(
+    0,
+    paddedPenceNumberString.length - 2
+  );
+  const pence = paddedPenceNumberString
+    .substring(paddedPenceNumberString.length - 2)
+    .padEnd(2, "0");
+
+  return `£${pounds}.${pence}`;
+}
+
+console.log(toPounds("399p")); // £3.99
+console.log(toPounds("9p")); // £0.09
+console.log(toPounds("50p")); // £0.50
+console.log(toPounds("1234p")); // £12.34
+console.log(toPounds("7p")); // £0.07
