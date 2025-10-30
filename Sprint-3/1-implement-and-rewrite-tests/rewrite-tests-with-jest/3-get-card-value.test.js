4d1c6ac9d1854d2b654044d2c105c66b60330a9c
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2025-10-30 13:08:52 +0100

    Update 3-get-card-value.test.js
    updated return values for different cases

--- Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
+++ Sprint-3/1-implement-and-rewrite-tests/rewrite-tests-with-jest/3-get-card-value.test.js
@@ -8,6 +8,27 @@ test("should return 11 for Ace of Spades", () => {
 });
 
 // Case 2: Handle Number Cards (2-10):
+test("should return correct numeric value for number cards", () => {
+  expect(getCardValue("2♦")).toEqual(2);
+  expect(getCardValue("7♣")).toEqual(7);
+  expect(getCardValue("10♥")).toEqual(10);
+});
+
 // Case 3: Handle Face Cards (J, Q, K):
+test("should return 10 for face cards (J, Q, K)", () => {
+  expect(getCardValue("J♠")).toEqual(10);
+  expect(getCardValue("Q♦")).toEqual(10);
+  expect(getCardValue("K♥")).toEqual(10);
+});
+
 // Case 4: Handle Ace (A):
+test("should return 11 for Ace cards (regardless of suit)", () => {
+  expect(getCardValue("A♣")).toEqual(11);
+  expect(getCardValue("A♦")).toEqual(11);
+});
+
 // Case 5: Handle Invalid Cards:
+test("should throw an error for invalid card rank", () => {
+  expect(() => getCardValue("Z♠")).toThrow("Invalid card rank");
+  expect(() => getCardValue("1♥")).toThrow("Invalid card rank");
+});
