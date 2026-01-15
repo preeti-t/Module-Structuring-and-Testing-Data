ed8715c9a20c1fd3515c4620c111c00c477af7f1
Preeti Tyagi <68293926+preeti-t@users.noreply.github.com>
2026-01-15 13:14:31 +0100

    Update get-ordinal-number.js
    implementation completed.

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,11 +1,13 @@
 function getOrdinalNumber(num) {
   if (num > 1 || num < 1) {
-    return "No 1st";
+    return num + " is not 1st";
   }
 
   return "1st";
 }
 
-console.log(getOrdinalNumber(1));
+console.log(getOrdinalNumber(1)); // "1st"
+console.log(getOrdinalNumber(2)); // "2 is not 1st"
+console.log(getOrdinalNumber(0)); // "0 is not 1st"
 
 module.exports = getOrdinalNumber;
